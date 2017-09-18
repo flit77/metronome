26e466f9469dfb7d21ff69a9ba303bcb428de536
Vasiliy Shubko <flit77@gmail.com>
2017-09-18 23:32:31 +0200

    eliminate unuzed code

--- src/App.js
+++ src/App.js
@@ -3,13 +3,11 @@ import logo from './logo.svg';
 import './App.css';
 import './index.css';
 import Metronome from './Metronome';
-import registerServiceWorker from './registerServiceWorker';
 
 class App extends Component {
   render() {
     return <Metronome />;
   }
 }
-registerServiceWorker();
 
 export default App;
