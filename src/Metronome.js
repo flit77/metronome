28e901d7245a8f6f25b304176dcd76075f8a2ac6
Vasiliy Shubko <flit77@gmail.com>
2017-09-19 22:06:14 +0200

    loop sound according to bpm

--- src/Metronome.js
+++ src/Metronome.js
@@ -19,7 +19,34 @@ class Metronome extends Component {
   }
 
   startStop = () => {
-    this.click1.play();
+    if (this.state.playing) {
+      clearInterval(this.timer);
+      this.setState({
+        playing: false
+      });
+    } else {
+      this.timer = setInterval(this.playClick, 60 / this.state.bpm * 1000);
+      this.setState(
+        {
+          count: 0,
+          playing: true
+        },
+        this.playClick
+      );
+    }
+  };
+
+  playClick = () => {
+    const { count, beatsPerMeasure } = this.state;
+
+    if (count % beatsPerMeasure === 0) {
+      this.click2.play();
+    } else {
+      this.click1.play();
+    }
+    this.setState(state => ({
+      count: (state.count + 1) % state.beatsPerMeasure
+    }));
   };
 
   handleBpmChange = event => {
